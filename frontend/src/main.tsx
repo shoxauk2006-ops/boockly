23116f5076a6f9db9a5f96d3192bb6727e306430
shoxauk2006-ops <shoxauk2006@gmail.com>
2026-09-24 07:21:11 +0500

    Add Vercel Web Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import './style.css';
 import { App } from './App';
+
+inject();
 
 createRoot(
   document.getElementById('root')!
